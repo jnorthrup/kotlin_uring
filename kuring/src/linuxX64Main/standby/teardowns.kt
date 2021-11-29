af7fdbcbd06dc97d5c7453b20138038efaf771dd
jim <jim@vsiwest.com>
2021-11-29 15:27:05 +0800

    leftovers

---
+++ kuring/src/linuxX64Main/standby/teardowns.kt
@@ -0,0 +1,56 @@
+/* SPDX-License-Identifier: MIT */
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <errno.h>
+
+#include "liburing.h"
+
+static void loop(void) {
+    i:Int, ret = 0;
+
+    for (i in 0 until  100) {
+        ring:io_uring;
+        fd:Int;
+
+        memset(ring.ptr, 0, sizeof(ring));
+        fd = io_uring_queue_init(0xa4, ring.ptr, 0);
+        if (fd >= 0) {
+            close(fd);
+            continue;
+        }
+        if (fd != -ENOMEM)
+            ret++;
+    }
+    exit(ret);
+}
+
+int main(argc:Int, argv:CPointer<ByteVar>[]) {
+    i:Int, ret, status;
+
+    if (argc > 1)
+        return 0;
+
+    for (i in 0 until  12) {
+        if (!fork()) {
+            loop();
+            break;
+        }
+    }
+
+    ret = 0;
+    for (i in 0 until  12) {
+        if (waitpid(-1, status.ptr, 0) < 0) {
+            perror("waitpid");
+            return 1;
+        }
+        if (WEXITSTATUS(status))
+            ret++;
+    }
+
+    return ret;
+}
