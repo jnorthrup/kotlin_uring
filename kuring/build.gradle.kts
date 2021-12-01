03a60b23a441114ab62d790d59d88f873cdf52d1
jim <jim@vsiwest.com>
2021-12-02 03:57:58 +0800

    new memory model

--- kuring/build.gradle.kts
+++ kuring/build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinMetadataTarget
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
 
 
-val coroutinesVersion = "1.5.2-native-mt"// "1.5.2"
+val coroutinesVersion = "1.5.1-new-mm-dev2"//"1.5.2-native-mt"// "1.5.2"
 val atomicfuVersion = "0.17.0"
 val statelyVersion = "1.2.0"
 
@@ -12,8 +12,11 @@ plugins {
     id("maven-publish")
     id("signing")
 }
- 
+
 repositories {
+   maven {
+        url = uri("https://maven.pkg.jetbrains.space/public/p/kotlinx-coroutines/maven")
+    }
     mavenCentral()
     gradlePluginPortal()
     google()
@@ -23,10 +26,17 @@ repositories {
 enum class test_bins { cat, fixedlink, iopoll, lfsopenat, link, opath, register, stdout, teardown, timeout, }
 
 kotlin {
-    linuxX64 { binaries { test_bins.values().forEach { executable(it.name, listOf(DEBUG/*, RELEASE*/)) { baseName = it.name;entryPoint = "test.$it.main" } } }
-         println( "compilations: ${compilations}")
-        compilations.first()     .cinterops {
-            println( "compilation: $this")
+    linuxX64 {
+        binaries {
+            test_bins.values().forEach {
+                executable(it.name, listOf(DEBUG/*, RELEASE*/)) {
+                    baseName = it.name;entryPoint = "test.$it.main"
+                }
+            }
+        }
+        println("compilations: ${compilations}")
+        compilations.first().cinterops {
+            println("compilation: $this")
 
             create(name) {
                 defFile = project.file("src/nativeInterop/cinterop/linux_uring.def")
@@ -36,6 +46,11 @@ kotlin {
     targets.withType<KotlinNativeTarget> {
         val main by compilations.getting {
             kotlinOptions.freeCompilerArgs += listOf("-Xopt-in=kotlin.RequiresOptIn")
+        }
+        binaries.all {
+                binaryOptions["memoryModel"] = "experimental"
+    freeCompilerArgs += "-Xruntime-logs=gc=info"
+            binaryOptions["freezing"] = "disabled"
         }
     }
 
@@ -53,6 +68,7 @@ kotlin {
             for (compilation in compilations) {
                 if (compilation.name == "nativeMain") {
                     compilation.kotlinOptions.freeCompilerArgs = listOf("-Xopt-in=kotlin.RequiresOptIn")
+
                 }
             }
         }
@@ -62,94 +78,93 @@ kotlin {
         val commonMain by getting {
             dependencies {//1.5.2-native-mt
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+//                implementation("org.jetbrains.kotlinx:atomicfu:$atomicfuVersion")
+//                implementation("co.touchlab:stately-common:$statelyVersion")  }
             }
-        }
-        val commonTest by getting {
-            dependencies {
-                implementation(kotlin("test"))
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
-                implementation("org.jetbrains.kotlinx:atomicfu:$atomicfuVersion")
-                implementation("co.touchlab:stately-common:$statelyVersion")
-            }
-        }
-        val nativeMain by creating {
-            dependsOn(commonMain)
-        }
-        val nativeTest by creating {
-            dependsOn(commonTest)
-        }
+            val commonTest by getting {
+                dependencies {
+                    implementation(kotlin("test"))
+                    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
 
-        /*
-        val appleMain by creating
-
-        val appleTargets = listOf(
-            "iosX64",
-            "iosArm64",
-            "iosArm32",
-            "iosSimulatorArm64",
-            "macosArm64",
-            "macosX64",
-            "tvosArm64",
-            "tvosSimulatorArm64",
-            "tvosX64",
-            "watchosArm32",
-            "watchosArm64",
-            "watchosSimulatorArm64",
-            "watchosX86"
-            // waiting on https://github.com/Kotlin/kotlinx.coroutines/pull/2679
-            //"watchosX64"
-        )
-        appleTargets.forEach {
-            getByName("${it}Main") {
-                dependsOn(appleMain)
+                }
             }
-        }*/
-
-        (/*appleTargets +*/ listOf(/*"mingwX64", */"linuxX64")).forEach {
-            getByName("${it}Main") {
-                dependsOn(nativeMain)
+            val nativeMain by creating
+            val nativeTest by creating {
+                dependsOn(commonTest)
             }
-            getByName("${it}Test") {
-                dependsOn(nativeTest)
+
+            /*
+            val appleMain by creating
+
+            val appleTargets = listOf(
+                "iosX64",
+                "iosArm64",
+                "iosArm32",
+                "iosSimulatorArm64",
+                "macosArm64",
+                "macosX64",
+                "tvosArm64",
+                "tvosSimulatorArm64",
+                "tvosX64",
+                "watchosArm32",
+                "watchosArm64",
+                "watchosSimulatorArm64",
+                "watchosX86"
+                // waiting on https://github.com/Kotlin/kotlinx.coroutines/pull/2679
+                //"watchosX64"
+            )
+            appleTargets.forEach {
+                getByName("${it}Main") {
+                    dependsOn(appleMain)
+                }
+            }*/
+
+            (/*appleTargets +*/ listOf(/*"mingwX64", */"linuxX64")).forEach {
+                getByName("${it}Main") {
+                    dependsOn(nativeMain)
+                }
+                getByName("${it}Test") {
+                    dependsOn(nativeTest)
+                }
             }
         }
     }
-}
 
-kotlin {
-    targets.all {
-        compilations.all {
-            // https://youtrack.jetbrains.com/issue/KT-46257
-            kotlinOptions.allWarningsAsErrors = false
+    kotlin {
+        targets.all {
+            compilations.all {
+                // https://youtrack.jetbrains.com/issue/KT-46257
+                kotlinOptions.allWarningsAsErrors = false
+            }
         }
     }
+
+    val ktlintConfig by configurations.creating
+
+    dependencies {
+        ktlintConfig("com.pinterest:ktlint:0.41.0")
+    }
+
+    val ktlint by tasks.registering(JavaExec::class) {
+        group = "verification"
+        description = "Check Kotlin code style."
+        classpath = ktlintConfig
+        main = "com.pinterest.ktlint.Main"
+        args = listOf("src/**/*.kt")
+    }
+
+    val ktlintformat by tasks.registering(JavaExec::class) {
+        group = "formatting"
+        description = "Fix Kotlin code style deviations."
+        classpath = ktlintConfig
+        main = "com.pinterest.ktlint.Main"
+        args = listOf("-F", "src/**/*.kt", "*.kts")
+    }
+
+    val checkTask = tasks.named("check")
+    checkTask.configure {
+        dependsOn(ktlint)
+    }
+
+    apply("publish.gradle")
 }
-
-val ktlintConfig by configurations.creating
-
-dependencies {
-    ktlintConfig("com.pinterest:ktlint:0.41.0")
-}
-
-val ktlint by tasks.registering(JavaExec::class) {
-    group = "verification"
-    description = "Check Kotlin code style."
-    classpath = ktlintConfig
-    main = "com.pinterest.ktlint.Main"
-    args = listOf("src/**/*.kt")
-}
-
-val ktlintformat by tasks.registering(JavaExec::class) {
-    group = "formatting"
-    description = "Fix Kotlin code style deviations."
-    classpath = ktlintConfig
-    main = "com.pinterest.ktlint.Main"
-    args = listOf("-F", "src/**/*.kt", "*.kts")
-}
-
-val checkTask = tasks.named("check")
-checkTask.configure {
-    dependsOn(ktlint)
-}
-
-apply("publish.gradle")
